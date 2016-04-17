5ca3a292db09395c684f17233e9a43367ca1ada3
Max Lutterbeck <max.lutterbeck@gmail.com>
2016-04-17 22:49:58 +0200

    update cwave config & add fog-aws gem

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,6 +7,9 @@ if Rails.env.production?
       :aws_secret_access_key_id    => ENV['S3_SECRET_KEY'],
       :region                      => ENV['S3_REGION']
     }
+
+    config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+
     config.fog_directory          = ENV['S3_BUCKET']
   end
 end
