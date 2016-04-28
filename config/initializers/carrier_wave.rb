67776251f77577e6c480e81720e052ed416f2f09
Max Lutterbeck <max.lutterbeck@gmail.com>
2016-04-28 21:55:38 +0200

    carrier wave...

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,11 +6,11 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3
       :provider                    => 'AWS',
-      :aws_access_key_id           => ENV["s3_access_key"],
-      :aws_secret_access_key       => ENV["s3_secret_key"],
-      :region                      => ENV["s3_region"]
+      :aws_access_key_id           => ENV["S3_ACCESS_KEY"],
+      :aws_secret_access_key       => ENV["S3_SECRET_KEY"],
+      :region                      => ENV["S3_REGION"]
     }
 
-    config.fog_directory          = ENV["s3_bucket"]
+    config.fog_directory          = ENV["S3_BUCKET"]
   end
 end
