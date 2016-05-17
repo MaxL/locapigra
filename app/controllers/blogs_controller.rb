aa05797f6ab349bf439251d583dd5647d117f236
Max Lutterbeck <max.lutterbeck@gmail.com>
2016-05-17 19:20:59 +0200

    fix blog errors, add favicon

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,8 @@
 class BlogsController < ApplicationController
   def index
     client = Tumblr::Client.new :consumer_key => 'VOf3IKabebfz5hnyCn8dY0JVVX80cqQ1Zc8xzd7TDgOSeCSE51'
-    response = client.posts 'locapigra.tumblr.com', limit: 50, reblog_info: true, notes_info: true
+    response = client.posts 'locapigra.tumblr.com', limit: 40, reblog_info: true, notes_info: true
+    puts response
     @posts = response.to_ostruct.posts.paginate(page: params[:page], per_page: 10)
   end
 end
