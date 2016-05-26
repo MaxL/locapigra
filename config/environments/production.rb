3c6a54fc2446dad872c017c161c0353539bcfab9
Max Lutterbeck <max.lutterbeck@gmail.com>
2016-05-26 22:38:03 +0200

    small fixes + errorpages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,11 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  config.assets.configure do |env|
+    env.register_transformer 'text/haml', 'text/html', Tilt::HamlTemplate
+    env.register_mime_type 'text/haml', extensions: ['.html']
+    env.register_engine '.haml', Tilt::HamlTemplate
+  end
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
