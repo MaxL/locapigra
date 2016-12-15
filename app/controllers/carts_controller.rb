01f864443a4f7174df36292727bd0f06752942a8
Max Lutterbeck <berlindisplay.max@gmail.com>
2016-12-15 17:01:41 +0100

    works just needs ui

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -60,18 +60,22 @@ class CartsController < ApplicationController
     @order.order_status_id = 2
     if @order.update_attributes(order_params)
       if @order.agreement
-        if @order.payment_choice.name == "Paypal"
-          create_braintree_payment @order.total, @order
-        end
-        @order.decrease_inventory
-        @order.save
-        session.delete :order_id
-        OrderMailer.confirmation_mail(@order).deliver_later
-        flash[:success] = "Order placed successfully"
-        if current_user
-          redirect_to confirm_order_path @order
+        @payment = PaymentChoice.find(@order.payment_choice_id)
+        if @payment.name == "Paypal"
+          @order.order_status_id = 6
+          @order.save
+          create_braintree_payment @order.total, @order, params["payment_method_nonce"]
         else
-          redirect_to thanks_path
+          @order.decrease_inventory
+          @order.save
+          session.delete :order_id
+          OrderMailer.confirmation_mail(@order).deliver_later
+          flash[:success] = "Order placed successfully"
+          if current_user
+            redirect_to confirm_order_path @order
+          else
+            redirect_to thanks_path
+          end
         end
       else
         flash[:danger] = "Your order could not be completed"
@@ -83,51 +87,59 @@ class CartsController < ApplicationController
     end
   end
 
+  def create_braintree_payment total, order, nonce
+    amount = total
+
+    result = Braintree::Transaction.sale(
+      amount: amount,
+      payment_method_nonce: nonce,
+      :options => {
+        :submit_for_settlement => true
+      }
+    )
+
+    if result.success? || result.transaction
+      validate_order result.transaction.id
+    else
+      error_messages = result.errors.map { |error| "Error: #{error.code}: #{error.message}" }
+      flash[:error] = error_messages
+      redirect_to cart_path
+    end
+  end
+
   private
     def order_params
       params.require(:order).permit(:total, :tax, :shipping, :address_id, :agreement, :payment_choice_id, address_attributes: [ :id, :recipient, :street, :city, :zip, :state, :country, :email ])
     end
 
-    def create_braintree_payment total, order
-      amount = total
-      nonce = params["payment_method_nonce"]
-
-      result = Braintree::Transaction.sale(
-        amount: amount,
-        payment_method_nonce: nonce,
-        :options => {
-          :submit_for_settlement => true
-        }
-      )
-
-      if result.success? || result.transaction
-        if current_user
-          redirect_to confirm_order_path @order
-        else
-          redirect_to thanks_path
-        end
-      else
-        error_messages = result.errors.map { |error| "Error: #{error.code}: #{error.message}" }
-        flash[:error] = error_messages
-        redirect_to cart_path
-      end
+    def validate_order transaction_id
+      @transaction = Braintree::Transaction.find(transaction_id.to_s)
+      @result = _create_result_hash(@transaction)
     end
 
     def _create_result_hash(transaction)
       status = transaction.status
 
       if TRANSACTION_SUCCESS_STATUSES.include? status
-        result_hash = {
-          :header => "Sweet Success!",
-          :icon => "success",
-          :message => "Your test transaction has been successfully processed. See the Braintree API response and try again."
-        }
+        @order.decrease_inventory
+        @order.order_status_id = 3
+        @order.save
+        session.delete :order_id
+        OrderMailer.confirmation_mail(@order).deliver_later
+        flash[:success] = "Order placed successfully"
+        if current_user
+          redirect_to confirm_order_path @order
+        else
+          redirect_to thanks_path
+        end
       else
         result_hash = {
           :header => "Transaction Failed",
           :icon => "fail",
           :message => "Your test transaction has a status of #{status}. See the Braintree API response and try again."
         }
+        flash[:error] = "Transaction failed: your transaction has a status of #{status}"
+        redirect_to cart_path
       end
     end
 
