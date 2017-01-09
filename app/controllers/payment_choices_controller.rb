964cd00141a871ec443082dc9449ddcea5f6bd95
Max Lutterbeck <max.lutterbeck@gmail.com>
2017-01-09 20:33:28 +0100

    fixed order mailer error and scoped payments

--- app/controllers/payment_choices_controller.rb
+++ app/controllers/payment_choices_controller.rb
@@ -2,7 +2,7 @@ class PaymentChoicesController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @payment_choices = PaymentChoice.all
+    @payment_choices = PaymentChoice.unscoped.all
   end
 
   def new
