dd2e1b4a2a0db89dc0dc118dc3b47e6485d292bd
maxl <max.lutterbeck@gmail.com>
2017-08-09 21:40:22 +0200

    try other approach to font assetts problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,6 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  config.assets.paths << Rails.root.join("app", "assets", "fonts")
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
