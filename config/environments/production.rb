3e047f846bddb529c413782a28f2690eea196db6
maxl <max.lutterbeck@gmail.com>
2019-05-01 09:53:43 +0200

    update mailgun config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-      api_key: ENV['MAILGUN_PRIVATE_KEY'],
+      api_key: ENV['MAILGUN_PUBLIC_KEY'],
       domain: ENV['MAILGUN_URL']
   }
 
